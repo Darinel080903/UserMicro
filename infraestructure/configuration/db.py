2ab51f482df58a8d6e548265bfd27f009cd30c31
Darinel080903 <darinelmr12345@gmail.com>
2024-07-20 15:49:32 -0600

    Add pool recycle

--- infraestructure/configuration/db.py
+++ infraestructure/configuration/db.py
@@ -10,7 +10,7 @@ connection = os.getenv('URR')
 user = os.getenv('USS')
 password = os.getenv('PSS')
 
-engine = create_engine(f'mysql+pymysql://{user}:{password}@{connection}:3306/users')
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{connection}:3306/users', pool_size=10, max_overflow=20, pool_recycle=300)
 meta = MetaData()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
